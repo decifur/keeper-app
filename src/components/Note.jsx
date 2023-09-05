32f9e652b4ea4d4d0e5c17e4292a20a5ff8f6102
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-09-05 21:07:42 +0530

    updated Note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return <div className="note">
     <h1>
-    This is the note title
+    {props.title}
     </h1>
     <p>
-    This is the note content
+    {props.content}
     </p>
 </div>
 }
